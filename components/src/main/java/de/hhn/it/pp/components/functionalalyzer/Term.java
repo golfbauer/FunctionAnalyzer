b13ea32070d3fef25b7076b879dd3f7341bcdc9c
Ansgar Koopmann <koopmann@stud.hs-heilbronn.de>
2020-10-21 16:59:21 +0200

    Add JavaDoc to class definition

--- components/src/main/java/de/hhn/it/pp/components/functionalalyzer/Term.java
+++ components/src/main/java/de/hhn/it/pp/components/functionalalyzer/Term.java
@@ -1,5 +1,8 @@
 package de.hhn.it.pp.components.functionalalyzer;
 
+/**
+ * Represent a single term inside a function
+ */
 public class Term {
   private double exponent;
   private double factor;
