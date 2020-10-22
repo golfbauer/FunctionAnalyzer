7a3dd9d6e3b04e2b297b64ce7a3e6d6b4af29afd
Yusuf Sancar Berber <yberber@stud.hs-heilbronn.de>
2020-10-23 00:10:48 +0200

    added Learning Cards (main class)

---
+++ components/src/test/java/de/hhn/it/pp/components/learningCards/LearningCards.java
@@ -0,0 +1,62 @@
+
+public class LearningCards implements ILearningCards{
+	
+	LearningCardManager lerM;
+	SessionManager sesM;
+
+	public LearningCards() {
+		lerM = new LearningCardManager();
+		sesM = new SessionManager();
+	}
+
+	// Method to create cardset
+	public void createCardSet() {
+		lerM.createCardSet();
+	}
+
+	// Method to remove cardset
+	public void removeCardSet(int cardSetIndex) {
+		lerM.removeCardset(cardSetIndex);
+	}
+
+	// Method to edit  title of the cardset
+	public void editCardSetTitle(int cardSetIndex) {
+		lerM.getCardset(cardSetIndex).setTitle();
+	}
+	
+	// Method to add card to cardset
+	public void addCardToCardSet(int cardSetIndex) {
+		lerM.getCardset(cardSetIndex).addCardtoSet(new Card());
+	}
+
+	// Method to add a card to cardset
+	public void addCardToCardSet(int cardSetIndex, Card card) {
+		lerM.getCardset(cardSetIndex).addCardtoSet(card);
+	}
+
+	// Method to add a Card to cardset
+	public void removeCardFromCardSet(int cardSetIndex, int cardIndex) {
+		lerM.getCardset(cardSetIndex).removeCardfromSet(cardIndex);
+	}
+
+	// Method to set the text of the card
+	public void editCardFromCardSet(int cardSetIndex, int cardIndex) {
+		lerM.getCardset(cardSetIndex).getCardfromSet(cardIndex).setText();
+	}
+
+	// Method to start learning session
+	public void startLearningSession(int cardSetIndex) {
+		sesM.startLearningSession(lerM.getCardset(cardSetIndex));
+
+	}
+
+	// Method to repeat unsolved and unseen Cards
+	public void repeatUnseenAndUnsolvedCards(int cardSetIndex) {
+		sesM.repeatUnseenAndUnsolvedCards(lerM.getCardset(cardSetIndex));
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
