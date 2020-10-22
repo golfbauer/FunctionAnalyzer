e63c75bbfe9c52f6e764c9140fbb0dd408bb016f
Yusuf Sancar Berber <yberber@stud.hs-heilbronn.de>
2020-10-23 00:04:42 +0200

    add Interface

---
+++ components/src/test/java/de/hhn/it/pp/components/learningCards/ILearningCards.java
@@ -0,0 +1,14 @@
+
+public interface ILearningCards {
+
+	void createCardSet();
+	void removeCardSet(int cardSetIndex);
+	void editCardSetTitle(int cardSetIndex);
+	void addCardToCardSet(int cardSetIndex);
+	void addCardToCardSet(int cardSetIndex, Card card);
+	void removeCardFromCardSet(int cardSetIndex, int cardIndex);
+	void editCardFromCardSet(int cardSetIndex, int cardIndex);
+	void startLearningSession(int cardSetIndex);
+	void repeatUnseenAndUnsolvedCards(int cardSetIndex);
+	
+}
