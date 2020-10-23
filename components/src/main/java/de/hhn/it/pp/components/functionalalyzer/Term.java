0dd39229466df10298b59da6f8ef2fcee5888145
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2020-10-23 09:34:20 +0200

    Add Constructors and Value-Field to Term

--- components/src/main/java/de/hhn/it/pp/components/functionalalyzer/Term.java
+++ components/src/main/java/de/hhn/it/pp/components/functionalalyzer/Term.java
@@ -4,7 +4,25 @@ package de.hhn.it.pp.components.functionalalyzer;
  * Represent a single term inside a function
  */
 public class Term {
-  private double exponent;
+  private double value;
+  private Term exponent;
   private double factor;
   private String variable;
+
+  public Term(Term exponent, double factor, String variable){
+    this.exponent = exponent;
+    this.factor = factor;
+    this.variable = variable;
+  }
+
+  public Term(Term exponent, double value){
+    this.exponent = exponent;
+    this.value = value;
+  }
+
+  public Term(double value){
+    this.value = value;
+  }
 }
+
+
