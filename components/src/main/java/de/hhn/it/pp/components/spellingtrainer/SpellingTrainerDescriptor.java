1c7068326fb11ae2f60f246efae2283277a424f2
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-10-24 13:33:14 +0200

    Formated Class SpellingTrainerDescriptor

--- components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerDescriptor.java
+++ components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerDescriptor.java
@@ -9,41 +9,43 @@ import java.util.HashMap;
 
 public class SpellingTrainerDescriptor {
 
-    private HashMap<String, File> audioFiles= new HashMap<>();
-    private ArrayList<String> spellingWords= new ArrayList<>();
-    private int counterWrongWords;
-    private int counterRightWords;
-    private int counterRemainingWords;
+  private HashMap<String, File> audioFiles = new HashMap<>();
+  private ArrayList<String> spellingWords = new ArrayList<>();
+  private int counterWrongWords;
+  private int counterRightWords;
+  private int counterRemainingWords;
 
-    public void setAudioFiles(String spellingWord,File audioFile){
-        audioFiles.put(spellingWord,audioFile);
+  public void setAudioFiles(String spellingWord, File audioFile) {
+    audioFiles.put(spellingWord, audioFile);
+  }
+
+  public File getAudioFile(String spellingWord) {
+    return audioFiles.get(spellingWord);
+  }
+
+  public void setSpellingWord(String spellingWord) {
+    spellingWords.add(spellingWord);
+  }
+
+  public String getSpellingWord(int index) {
+    return spellingWords.get(index);
+  }
+
+  public int getCounter(String counterName) throws CounterNotFoundException {
+
+    switch (counterName.toLowerCase()) {
+      case "wrong":
+        return counterWrongWords;
+
+      case "right":
+        return counterRightWords;
+
+      case "remaining":
+        return counterRemainingWords;
+
+      default:
+        throw new CounterNotFoundException();
+
     }
-    public File getAudioFile(String spellingWord){
-        return audioFiles.get(spellingWord);
-    }
-    public void setSpellingWord(String spellingWord){
-        spellingWords.add(spellingWord);
-    }
-    public String getSpellingWord(int index){
-        return spellingWords.get(index);
-    }
-    public int getCounter(String counterName) throws CounterNotFoundException{
-
-        switch(counterName.toLowerCase()){
-            case "wrong":
-                return counterWrongWords;
-
-            case "right":
-                return counterRightWords;
-
-            case "remaining":
-                return counterRemainingWords;
-
-            default:
-                 throw new CounterNotFoundException();
-
-
-        }
-    }
-
+  }
 }
