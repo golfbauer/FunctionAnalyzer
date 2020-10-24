8aef9a7802a2dda5bdf74ffd44892b2b7f3c6c17
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-10-24 13:34:06 +0200

    Formated Class SpellingTrainerService

--- components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerService.java
+++ components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerService.java
@@ -7,33 +7,37 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
- *The SpellingTrainerInterface shows the basic functionalities of the Spellingtrainer
+ * The SpellingTrainerInterface shows the basic functionalities of the Spellingtrainer.
  */
 public interface SpellingTrainerService {
 
-    /**
-     * Plays the audio file
-     * @param audioFile Audiofile of the word the user needs to spell
-     */
-    void audioOutput(File audioFile) throws FileNotFoundException;
+  /**
+   * Plays the audio file.
+   *
+   * @param audioFile Audiofile of the word the user needs to spell
+   */
+  void audioOutput(File audioFile) throws FileNotFoundException;
 
-    /**
-     * Checks the spelling of the entered word
-     * @param writtenWord word that was entered by the user
-     * @return Returns true if the spelling of the word was correct, false if the spelling was wrong
-     */
-    boolean checkSpelling(String writtenWord);
+  /**
+   * Checks the spelling of the entered word.
+   *
+   * @param writtenWord word that was entered by the user
+   * @return Returns true if the spelling of the word was correct, false if the spelling was wrong
+   */
+  boolean checkSpelling(String writtenWord);
 
-    /**
-     * Adds the given word to the Learning List and maps the audio file to the word
-     * @param word word that will be added
-     * @param audio audio that will be mapped to the word
-     */
-    void addWord(String word, File audio) throws WordAlreadyAddedException, FileNotFoundException;
+  /**
+   * Adds the given word to the Learning List and maps the audio file to the word.
+   *
+   * @param word  word that will be added
+   * @param audio audio that will be mapped to the word
+   */
+  void addWord(String word, File audio) throws WordAlreadyAddedException, FileNotFoundException;
 
-    /**
-     * Removes the given word from the Learning List and removes the mapping
-     * @param word word that is to be removed
-     */
-    void deleteWord(String word) throws WordNotFoundException;
+  /**
+   * Removes the given word from the Learning List and removes the mapping.
+   *
+   * @param word word that is to be removed
+   */
+  void deleteWord(String word) throws WordNotFoundException;
 }
