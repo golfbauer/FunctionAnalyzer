0f061db357351d40f224188245e1d6eb1f154ac0
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-10-27 18:18:15 +0100

    sequence of events

--- components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
@@ -3,8 +3,73 @@ package de.hhn.it.pp.components.vocabletrainer;
 public class DemoVocableTrainerUsage {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(DemoVocableTrainerUsage.class);
-  
+
   public static void main(String[] args) {
+      // - Benutzer, Score, Benutzerliste anzeigen
+    // Benutzer hinzufügen
+    // - Benutzerbearbeitung
+    // Bestätigen
+    // - Benutzer, Score, Benutzerliste
+    // Benutzer ändern
+    // - Benutzerbearbeitung
+    // Benutzer auswählen
+    // Betätigen
+    // - Benutzer, Score, Kategorieliste
+    // Kategorie hinzufügen
+    // - Kategoriebearbeitung
+    // SaveAndNew
+    // 2. Kategorie hinzufügen
+    // - Kategoriebearbeitung
+    // Save
+    // 2. Kategorie ändern
+    // - Kategoriebearbeitung
+    // Save
+    // - Benutzer, Score, Kategorieliste
+    // 1. Kategorie auswählen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // 1. Vokabel hinzufügen
+    // - Vokabelbearbeitung
+    // Save and New
+    // 2. Vokabel hinzufügen
+    // - Vokabelbearbeitung
+    // Save and New
+    // 3. Vokabel hinzufügen
+    // - Vokabelbearbeitung
+    // Save
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // 2. Vokabel ändern
+    // - Vokabelbearbeitung
+    // Bestätigen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // 3. Vokabel löschen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // Vokabeln lernen
+    // 1. Vokabel prüfen
+    // 2. Vokabel skip
+    // - Score, Vokabeln, Kategorie
+    // Auswertung bestätigen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // Vokabeln lernen
+    // lernen abbrechen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // Zurück
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // Vokabel hinzufügen
+    // - Vokabelbearbeitung
+    // Abbrechen
+    // - Benutzer, Score, Kategorie, Vokabelliste
+    // Zurück
+    // - Benutzer, Score, Kategorieliste
+    // Kategorie hinzufügen
+    // - Vokabelbearbeitung
+    // Abbrechen
+    // - Benutzer, Score, Kategorieliste
+    // Zurück
+    // - Benutzer, Score, Benutzerliste
+    // Benutzer hinzufügen
+    // - Benutzerbearbeitung
+    // Abbrechen
+
     // Homepage
     // pick -> add, choose (edit, delete) user, OK;
     // show -> User list (score list), (selected) user, score
