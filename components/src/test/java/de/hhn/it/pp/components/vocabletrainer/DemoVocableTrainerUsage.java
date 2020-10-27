6f5ba503ba67405d9d40cb4082243a14e7e3ca5f
bkammer <bkammer@stud.hs-heilbronn.de>
2020-10-27 12:42:06 +0100

    Idea of the demoproramm

--- components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
@@ -1,4 +1,43 @@
 package de.hhn.it.pp.components.vocabletrainer;
 
 public class DemoVocableTrainerUsage {
+  public static void main(String[] args) {
+    // Startseite
+    // auswahl -> Benutzer hinzufügen, auswählen (editieren, löschen), OK;
+    // anzeige -> Benutzerliste (Scoreliste), (ausgewählter) Benutzer, Score
+
+    // Kategorie Seite
+    // auswahl -> Kategorie hinzufügen, auswählen (editieren, löschen), OK, Back;
+    // anzeige -> Kategorieliste, Benutzer, Score
+
+    // Vokabel Seite
+    // auswahl -> Vokabel hinzufügen, auswählen (editieren, löschen); Kategorie (Set) lernen alles / falsch, Back;
+    // anzeige -> Ausgewählte Kategorie, Vokabelliste, anzeige ob vokabel schon gelernt, falsch
+    //  oder richtig beantwortet wurde, Benutzer, Score
+
+    // Benutzer editieren (hinzufügen)
+    // auswahl -> Textfeld (Name), OK, Cancle;
+    // anzeige -> Benutzer Name
+
+    // Kategorie editieren (hinzufügen)
+    // auswahl -> Textfeld (Name), OK; Cancle;
+    // anzeige -> Kategorie name
+
+    // Vokabel editieren (hinzufügen)
+    // auswahl -> Textfeld (origin), Textfeld (Übersetzung) , OK, Cancle;
+    // anzeige -> Vokabel name, Kategorie
+
+    // Lern seite
+    // auswahl -> Textfeld, OK, Skip, Cancle;
+    // anzeige -> Vokabel origin, Benutzer, Score, (bei skip -> Lösung)
+
+    // Fertig mit lernen
+    // auswahl -> OK
+    // anzeige -> Benutzer, neuer Score, falsche/richtige Wörter
+
+    // Löschen Seite
+    // auswahl OK, Cancle;
+    // anzeige -> "Bist du dir sicher?"
+
+  }
 }
