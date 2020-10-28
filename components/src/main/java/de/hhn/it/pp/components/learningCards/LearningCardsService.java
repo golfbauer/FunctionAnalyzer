c9a3a357bce5ef13a97afe56da5afa7e4a516b1f
yberber <yberber@stud.hs-heilbronn.de>
2020-10-29 00:01:11 +0100

    added exception classes

--- components/src/main/java/de/hhn/it/pp/components/learningCards/LearningCardsService.java
+++ components/src/main/java/de/hhn/it/pp/components/learningCards/LearningCardsService.java
@@ -1,20 +1,26 @@
 package de.hhn.it.pp.components.learningCards;
 
+import de.hhn.it.pp.components.exceptions.CardNotFoundException;
+import de.hhn.it.pp.components.exceptions.CardsetNotFoundException;
+
 public interface LearningCardsService {
 
     void createCardSet(String cardsetTitle);
 
-    void removeCardSet(int cardsetIndex);
+    void removeCardSet(int cardsetIndex) throws CardsetNotFoundException;
 
-    void addCardToCardSet(int cardsetIndex, String cardHeadline, String cardText);
+    void addCardToCardSet(int cardsetIndex, String cardHeadline, String cardText)
+            throws CardsetNotFoundException, CardNotFoundException;
 
-    void addCardToCardSet(int cardSetIndex, String cardText);
+    void addCardToCardSet(int cardSetIndex, String cardText) throws CardsetNotFoundException;
 
-    void removeCardFromCardSet(int cardSetIndex, int cardIndex);
+    void removeCardFromCardSet(int cardSetIndex, int cardIndex)
+            throws CardsetNotFoundException, CardNotFoundException;
 
-    void editCardFromCardSet(int cardSetIndex, int cardIndex, String newCardText);
+    void editCardFromCardSet(int cardSetIndex, int cardIndex, String newCardText)
+            throws CardsetNotFoundException, CardNotFoundException;
 
-    void startLearningSession(int cardSetIndex);
+    void startLearningSession(int cardSetIndex) throws CardsetNotFoundException;
 
-    void repeatUnseenAndUnsolvedCards(int cardSetIndex);
+    void repeatUnseenAndUnsolvedCards(int cardSetIndex) throws CardsetNotFoundException;
 }
--- components/src/test/java/de/hhn/it/pp/components/learningCards/DemoLearningCardsUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/learningCards/DemoLearningCardsUsage.java
@@ -1,8 +1,11 @@
 package de.hhn.it.pp.components.learningCards;
 
+import de.hhn.it.pp.components.exceptions.CardNotFoundException;
+import de.hhn.it.pp.components.exceptions.CardsetNotFoundException;
+
 public class DemoLearningCardsUsage {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CardsetNotFoundException, CardNotFoundException {
 
         LearningCardsService service = new LearningCardsService() {
             @Override
@@ -11,40 +14,41 @@ public class DemoLearningCardsUsage {
             }
 
             @Override
-            public void removeCardSet(int cardsetIndex) {
+            public void removeCardSet(int cardsetIndex) throws CardsetNotFoundException {
 
             }
 
             @Override
-            public void addCardToCardSet(int cardsetIndex, String cardHeadline, String cardText) {
+            public void addCardToCardSet(int cardsetIndex, String cardHeadline, String cardText) throws CardsetNotFoundException, CardNotFoundException {
 
             }
 
             @Override
-            public void addCardToCardSet(int cardSetIndex, String cardText) {
+            public void addCardToCardSet(int cardSetIndex, String cardText) throws CardsetNotFoundException {
 
             }
 
             @Override
-            public void removeCardFromCardSet(int cardSetIndex, int cardIndex) {
+            public void removeCardFromCardSet(int cardSetIndex, int cardIndex) throws CardsetNotFoundException, CardNotFoundException {
 
             }
 
             @Override
-            public void editCardFromCardSet(int cardSetIndex, int cardIndex, String newCardText) {
+            public void editCardFromCardSet(int cardSetIndex, int cardIndex, String newCardText) throws CardsetNotFoundException, CardNotFoundException {
 
             }
 
             @Override
-            public void startLearningSession(int cardSetIndex) {
+            public void startLearningSession(int cardSetIndex) throws CardsetNotFoundException {
 
             }
 
             @Override
-            public void repeatUnseenAndUnsolvedCards(int cardSetIndex) {
+            public void repeatUnseenAndUnsolvedCards(int cardSetIndex) throws CardsetNotFoundException {
 
             }
         };
+
 
         service.createCardSet("Pupulations");
         service.addCardToCardSet(0,"The population of Berlin is 3,562,000");
