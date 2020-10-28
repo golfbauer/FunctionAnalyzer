173a10ce0442068f7df74b6ad4453760650786a2
Tobias Maraci <tmaraci@stud.hs-heilbronn.de>
2020-10-28 10:13:13 +0100

    Comments translated to english

--- components/src/main/java/de/hhn/it/pp/components/typingtrainer/TypingTrainerService.java
+++ components/src/main/java/de/hhn/it/pp/components/typingtrainer/TypingTrainerService.java
@@ -5,60 +5,60 @@ import java.io.FileNotFoundException;
 
 /***
  * @author Tobias Maraci, Robert Pistea
- * @version 1.0
+ * @version 1.01
  */
 
 public interface TypingTrainerService {
   /***
-   * Überprüft das geschriebene Wort auf Richtigkeit
-   * @param word: Wort das überprüft werden soll
-   * @return : true, wenn Wort richtig ist
+   * Checks the written Word if its correct
+   * @param word: word that needs to be checked
+   * @return : true, if its right
    * @throws WordNotFoundException
    */
   boolean checkWord(String word) throws WordNotFoundException;
 
   /***
-   * Spielt einen Ton ab, wenn Wort falsch ist.
-   * @param soundFile: Ton der abgespielt wird
+   * Plays a sound, if the word is wrong
+   * @param soundFile: Sound that gets played
    * @throws FileNotFoundException
    */
   void audioOutput(File soundFile) throws FileNotFoundException;
 
   /***
-   * Markiert ein Wort, welches richtig geschrieben ist
-   * @param word: Wort das markiert wird
+   * Marks a word that is written correct
+   * @param word: Word that gets marked
    * @throws WordNotFoundException
    */
   void markWord(String word) throws WordNotFoundException;
 
   /***
-   * Wählt Text aus der geübt werden soll
-   * @param selectedText: Ausgewählter Text
+   * Selects the text that you want to practice
+   * @param selectedText: Selected Text
    */
   void selectionOfText(String selectedText);
 
   /***
-   * Beendet eine Übungsphase.
+   * Quits a learning session
    */
   void quitSession();
 
   /***
-   * Zeigt Feedback an
-   * @param time Gesamtzeit zum absolvieren
-   * @param wordsPerMinute Wörter die in der Minute richtig geschrieben wurden
+   * Shows Feedback
+   * @param time Time for completion
+   * @param wordsPerMinute Words that are correct in a minute
    */
   void showFeedback(long time, float wordsPerMinute);
 
   /***
-   * Speichert den Fortschritt des Users in eine txt File
-   * @param saveFile: File in der es gespeichert wird
+   * Saves the Progress of the User in a text file
+   * @param saveFile: File in which it gets saved
    * @throws FileNotFoundException
    */
   void saveScore(File saveFile) throws FileNotFoundException;
 
   /***
-   * Lädt die txt File falls sie existiert
-   * @param saveFile: File die geladen werden soll
+   * Loads die txt file, if she exists
+   * @param saveFile: File that gets loaded
    * @throws FileNotFoundException
    */
   void loadScore(File saveFile) throws FileNotFoundException;
