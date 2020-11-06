3a7ac9922ca36e1b5114146e8821054e4620c2c2
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-11-06 10:24:47 +0100

    comments deleted in demo

--- components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
+++ components/src/test/java/de/hhn/it/pp/components/vocabletrainer/DemoVocableTrainerUsage.java
@@ -253,43 +253,5 @@ public class DemoVocableTrainerUsage {
       logger.debug("Failed to remove the Category");
     }
 
-
-    // Homepage
-    // pick -> add, choose (edit, delete) user, OK;
-    // show -> User list (score list), (selected) user, score
-
-    // Category page
-    // pick -> add, choose (edit, delete) category, OK, Back;
-    // show -> Category list, user, score
-
-    // Vocabulary page
-    // pick -> add, choose (edit, delete) vocabulary; Category (Set) learn everything / wrong, back;
-    // show -> Selected category, vocabulary list, indication of whether vocabulary has already been learned, answered
-    // incorrectly or correctly, user, score
-
-    // edit (add) User
-    // pick -> Text field (name), OK, Cancel;
-    // show -> User name
-
-    // edit (add) Category
-    // pick -> Text field (name), OK; Cancel;
-    // show -> Category name
-
-    // edit (add) Vocabulary
-    // pick -> Text field (untranslated), text field (translation) , OK, Cancel;
-    // show -> Vocabulary name, category
-
-    // Learn page
-    // pick -> Text field, OK, Skip, Cancel;
-    // show -> Vocabulary untranslated, user, score, (at skip -> answer)
-
-    // Done with learning
-    // pick -> OK
-    // show -> User, new Score, wrong/correct words
-
-    // Delete page
-    // pick -> OK, Cancel;
-    // show -> "Are you sure?"
-
   }
 }
