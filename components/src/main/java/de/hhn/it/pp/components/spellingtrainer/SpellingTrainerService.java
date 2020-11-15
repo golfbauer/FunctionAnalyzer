6ca11ebc862d1e7ec3d20f5de6c3fc4b85a5ab48
Dennis Spohrer <dennis.spohrer123@gmail.com>
2020-11-15 19:31:04 +0100

    Added methods to set and get learning sets, start and end learning sessions and to play a next word

--- components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerService.java
+++ components/src/main/java/de/hhn/it/pp/components/spellingtrainer/SpellingTrainerService.java
@@ -5,6 +5,7 @@ import de.hhn.it.pp.components.spellingtrainer.exceptions.WordNotFoundException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 /**
  * The SpellingTrainerInterface shows the basic functionalities of the Spellingtrainer.
@@ -37,4 +38,57 @@ public interface SpellingTrainerService {
    * @param learningSet set, that the word is deleted from
    */
   void deleteWord(String word, LearningSet learningSet) throws WordNotFoundException;
+
+  /**
+   * Method to create a new learning set.
+   *
+   * @param learningSetName name of the learning set
+   * @return new learning set
+   */
+  LearningSet createLearningSet(String learningSetName);
+
+  /**
+   * Removes an learning set from the list of learning sets.
+   *
+   * @param learningSet learning set to be removed from the list of learning sets
+   */
+  void removeLearningSet(LearningSet learningSet);
+
+  /**
+   * Method to get all learning sets.
+   *
+   * @return list of learning sets
+   */
+  ArrayList<LearningSet> getLearningSets();
+
+  /**
+   * Method get a learning set by it's name.
+   *
+   * @param learningSetName name of the learning set
+   * @return learning set with associated name
+   */
+  LearningSet getLearningSet(String learningSetName);
+
+  /**
+   * Method to start an learning session with an learning set.
+   *
+   * @param learningSet set to be learned
+   * @return true if the learning session started successfully, false if it doesn't
+   */
+  boolean startLearning(LearningSet learningSet);
+
+
+  /**
+   * Method to stop a learning session.
+   *
+   * @return true if it was stopped successfully, false when not
+   */
+  boolean endLearning();
+
+  /**
+   * Method to play the next word.
+   */
+  void nextWord();
+
+
 }
