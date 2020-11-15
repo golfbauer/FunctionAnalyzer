04bf2c405d1bfd4d20df37e3ff442cb599bc6690
Dennis Spohrer <dennis.spohrer123@gmail.com>
2020-11-15 19:29:19 +0100

    Added methods to register and deregister an media presentation listener

--- components/src/main/java/de/hhn/it/pp/components/spellingtrainer/MediaPresentationListener.java
+++ components/src/main/java/de/hhn/it/pp/components/spellingtrainer/MediaPresentationListener.java
@@ -1,16 +1,43 @@
 package de.hhn.it.pp.components.spellingtrainer;
 
+import java.util.ArrayList;
+
 /**
  * Class MediaPresentationListener contains methods to react to triggered events from the user.
  */
 public class MediaPresentationListener {
+
+  static ArrayList<MediaPresentationListener> mediaPresentationListeners =
+      new ArrayList<>();
+
   /**
    * Method to present a media file.
    *
    * @param mediaReference reference to media file to be presented
    */
-  public void present(MediaReference mediaReference){
+  public void present(MediaReference mediaReference) {
 
   }
+
+  /**
+   * Method to register an media presentation listener.
+   *
+   * @param mediaPresentationListener media presentation listener to be registered
+   */
+  public static void registerMediaPresentationListener(
+      MediaPresentationListener mediaPresentationListener) {
+    mediaPresentationListeners.add(mediaPresentationListener);
+  }
+
+  /**
+   * Method to deregister and media presentation listener.
+   *
+   * @param mediaPresentationListener media presentation listener to be deregistered.
+   */
+  public static void deregisterMediaPresentationListener(
+      MediaPresentationListener mediaPresentationListener) {
+    mediaPresentationListeners.remove(mediaPresentationListener);
+  }
+
 
 }
