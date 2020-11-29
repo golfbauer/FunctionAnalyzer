ab5085d3c51ee62b39c94625127e6a0e600d5520
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-11-29 13:00:30 +0100

    implemented method present

--- components/src/main/java/de/hhn/it/pp/components/spellingtrainer/Provider/MediaPresentationListener.java
+++ components/src/main/java/de/hhn/it/pp/components/spellingtrainer/Provider/MediaPresentationListener.java
@@ -1,6 +1,11 @@
 package de.hhn.it.pp.components.spellingtrainer.Provider;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 /**
  * Class MediaPresentationListener contains methods to react to triggered events from the user.
@@ -15,8 +20,10 @@ public class MediaPresentationListener {
    *
    * @param mediaReference reference to media file to be presented
    */
-  public void present(MediaReference mediaReference) {
-
+  public void present(MediaReference mediaReference) throws Exception{
+      AudioInputStream inputStream = AudioSystem.getAudioInputStream(mediaReference.getMediaFile());
+      Clip clip = AudioSystem.getClip();
+      clip.open(inputStream);
   }
 
   public static MediaPresentationListener getMediaPresentationListener(int index){
