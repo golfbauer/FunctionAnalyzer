a68301788b33b0fd8b4cc9680cb72a3dfab143c5
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-11-30 15:29:37 +0100

    Added junit package and added TestSgdsSpellingTrainerServiceBadCases, TestSgdsSpellingTrainerServiceGoodCases test classes

---
+++ components/src/test/java/de/hhn/it/pp/components/spellingtrainer/junit/TestSgdsSpellingTrainerServiceBadCases.java
@@ -0,0 +1,6 @@
+package de.hhn.it.pp.components.spellingtrainer.junit;
+
+public class TestSgdsSpellingTrainerServiceBadCases {
+  private static final org.slf4j.Logger logger =
+      org.slf4j.LoggerFactory.getLogger(TestSgdsSpellingTrainerServiceBadCases.class);
+}
---
+++ components/src/test/java/de/hhn/it/pp/components/spellingtrainer/junit/TestSgdsSpellingTrainerServiceGoodCases.java
@@ -0,0 +1,6 @@
+package de.hhn.it.pp.components.spellingtrainer.junit;
+
+public class TestSgdsSpellingTrainerServiceGoodCases {
+  private static final org.slf4j.Logger logger =
+      org.slf4j.LoggerFactory.getLogger(TestSgdsSpellingTrainerServiceGoodCases.class);
+}
