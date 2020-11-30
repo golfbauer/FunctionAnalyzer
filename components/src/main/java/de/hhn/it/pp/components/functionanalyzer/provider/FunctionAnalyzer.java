dd0fc881e59fe9cabad9b7e8479dcc7ef698f4c7
Ansgar Koopmann <koopmann@stud.hs-heilbronn.de>
2020-11-30 16:47:26 +0100

    First try to implement readFunction method, not complete yet

--- components/src/main/java/de/hhn/it/pp/components/functionanalyzer/provider/FunctionAnalyzer.java
+++ components/src/main/java/de/hhn/it/pp/components/functionanalyzer/provider/FunctionAnalyzer.java
@@ -1,8 +1,12 @@
 package de.hhn.it.pp.components.functionanalyzer.provider;
 
+import java.util.ArrayList;
 import java.util.List;
 import de.hhn.it.pp.components.functionanalyzer.Function;
 import de.hhn.it.pp.components.functionanalyzer.FunctionAnalyserService;
+import de.hhn.it.pp.components.functionanalyzer.FunctionElement;
+import de.hhn.it.pp.components.functionanalyzer.Operator;
+import de.hhn.it.pp.components.functionanalyzer.Term;
 import de.hhn.it.pp.components.functionanalyzer.exceptions.ValueNotDefinedException;
 
 public class FunctionAnalyzer implements FunctionAnalyserService {
@@ -10,6 +14,41 @@ public class FunctionAnalyzer implements FunctionAnalyserService {
 
   @Override
   public Function readFunction(String input) throws IllegalArgumentException {
+    input = input.replaceAll("\\s", "");
+    ArrayList<String> terms = new ArrayList<>();
+    int lastTerm = 0;
+    for (int i = 0; i < input.length(); i++){
+      if (input.charAt(i) == '+' || input.charAt(i) == '-' && i != 0){
+        terms.add((input.charAt(lastTerm) != '-' && input.charAt(lastTerm) != '+' ? "+" : "") +
+            input.substring(lastTerm, i));
+        lastTerm = i;
+      }
+    }
+    terms.add(input.substring(lastTerm));
+    return null;
+  }
+
+  public FunctionElement translate(String input){
+    String buffer = "";
+    boolean klammer = false;
+    for(int i = 1; i < input.length(); i++) {
+      switch(input.charAt(i)){
+        case('('): klammer = true;
+        case('*'): ;
+        case('/'): ;
+        case('x'):
+          if(buffer == ""){
+            double val = 1;
+          }else {
+            Term term = new Term(new Term(1), Double.parseDouble(buffer), "x");
+          }
+        case('y'): ;
+        case('^'): ;
+        case(')'): klammer = false;
+        default: buffer += input.charAt(i);
+      }
+    }
+
     return null;
   }
 
