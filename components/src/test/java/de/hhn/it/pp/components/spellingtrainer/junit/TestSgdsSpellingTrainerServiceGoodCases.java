cb5a6a1075c9c101b598832e096af1e98517f719
Dennis Spohrer <dennis.spohrer123@gmail.com>
2020-12-01 12:59:10 +0100

    Added test methods for adding a word and deleting an word

--- components/src/test/java/de/hhn/it/pp/components/spellingtrainer/junit/TestSgdsSpellingTrainerServiceGoodCases.java
+++ components/src/test/java/de/hhn/it/pp/components/spellingtrainer/junit/TestSgdsSpellingTrainerServiceGoodCases.java
@@ -1,18 +1,19 @@
 package de.hhn.it.pp.components.spellingtrainer.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import de.hhn.it.pp.components.spellingtrainer.Provider.LearningEntry;
 import de.hhn.it.pp.components.spellingtrainer.Provider.MediaPresentationListener;
 import de.hhn.it.pp.components.spellingtrainer.Provider.SgdsSpellingTrainerService;
-import de.hhn.it.pp.components.spellingtrainer.SpellingTrainerDescriptor;
 import de.hhn.it.pp.components.spellingtrainer.exceptions.LearningSetCouldNotBeFoundException;
 import de.hhn.it.pp.components.spellingtrainer.exceptions.LearningSetNameAlreadyAssignedException;
 import de.hhn.it.pp.components.spellingtrainer.exceptions.WordAlreadyAddedException;
 import de.hhn.it.pp.components.spellingtrainer.exceptions.WordNotFoundException;
 import java.io.File;
-import org.junit.jupiter.api.AfterEach;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,38 +22,50 @@ import org.junit.jupiter.api.Test;
 public class TestSgdsSpellingTrainerServiceGoodCases {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(TestSgdsSpellingTrainerServiceGoodCases.class);
+  SgdsSpellingTrainerService service;
+  File audioFile;
 
-
-
+  @BeforeEach
+  void initialize()
+      throws LearningSetNameAlreadyAssignedException, LearningSetCouldNotBeFoundException,
+      WordAlreadyAddedException {
+    service = new SgdsSpellingTrainerService();
+    audioFile =
+        new File("src/main/java/de/hhn/it/pp/components/spellingtrainer/audiofiles/Book.wav");
+    service.createLearningSet("TestSet");
+    service.addWord("test", audioFile, "TestSet");
+    MediaPresentationListener mpl = new MediaPresentationListener();
+    service.startLearning("TestSet");
+  }
 
 
   @Test
   @DisplayName("Checks if the spelling is correct.")
-  void checkingSpellingIsCorrect()
-      throws LearningSetNameAlreadyAssignedException, LearningSetCouldNotBeFoundException,
-      WordAlreadyAddedException {
-    SgdsSpellingTrainerService service = new SgdsSpellingTrainerService();
-    File audioFile = new File("../../../../../../../../../main/java/de/hhn/it/pp/components/spellingtrainer/audiofiles/Book.wav");
-    service.createLearningSet("TestSet");
-    service.addWord("test", audioFile, "TestSet");
-    MediaPresentationListener mpl= new MediaPresentationListener();
-    service.startLearning("TestSet");
+  void checkingSpellingIsCorrect() {
     assertTrue(service.checkSpelling("test"));
   }
 
   @Test
   @DisplayName("Adds a word to an existing learningset")
-  void addWordToList() throws LearningSetCouldNotBeFoundException, WordAlreadyAddedException,
-      LearningSetNameAlreadyAssignedException {
-    SgdsSpellingTrainerService service = new SgdsSpellingTrainerService();
-
-    //TODO: Files global initialisieren
-    File audioFile = new File("../../../../../../../../../main/java/de/hhn/it/pp/components/spellingtrainer/audiofiles/Book.wav");
-    MediaPresentationListener mpl= new MediaPresentationListener();
-    service.createLearningSet("TestSet");
-    service.addWord("test2", audioFile, "TestSet");
-    service.startLearning("TestSet");
-    assertEquals("test2", SpellingTrainerDescriptor.getActiveLearningSet().getLearningEntries().get(0).getWordEntry());
+  void addWordToList() throws LearningSetCouldNotBeFoundException, WordAlreadyAddedException {
+    service.addWord("test2",audioFile,"TestSet");
+    assertEquals("test2",
+        service.getDescriptor().getActiveLearningSet().getLearningEntries().get(1).getWordEntry());
   }
+
+  @Test
+  @DisplayName("Deletes a word from an existing learning set.")
+  void deleteWordFromList() throws LearningSetCouldNotBeFoundException, WordNotFoundException {
+    service.deleteWord("test","TestSet");
+    boolean contains=false;
+        ArrayList<LearningEntry> leList= service.getLearningSet("TestSet").getLearningEntries();
+        for(LearningEntry le: leList ){
+          contains=le.getWordEntry().equals("test");
+        }
+        logger.debug("There are no learning entries.");
+        assertFalse(contains);
+
+  }
+
 
 }
