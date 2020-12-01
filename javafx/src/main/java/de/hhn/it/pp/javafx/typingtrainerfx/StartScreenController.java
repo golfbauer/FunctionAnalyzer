927a76890f4c761ec5cb0459023bd7d9adecef2e
Robert Pistea <robert.pistea@gmail.com>
2020-12-01 19:29:49 +0100

    Created Controller and GUITypingTrainer

---
+++ javafx/src/main/java/de/hhn/it/pp/javafx/typingtrainerfx/StartScreenController.java
@@ -0,0 +1,21 @@
+package de.hhn.it.pp.javafx.typingtrainerfx;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox; //?!?!?!
+import javafx.stage.Stage;
+
+public class StartScreenController implements Initializable {
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+}
---
+++ javafx/src/main/java/de/hhn/it/pp/javafx/typingtrainerfx/TypingScreenController.java
@@ -0,0 +1,20 @@
+package de.hhn.it.pp.javafx.typingtrainerfx;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox; //?!?!?!
+import javafx.stage.Stage;
+
+public class TypingScreenController implements Initializable {
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+}
