a506a6f5357431b6e64e6197dccfb6dfa8ab6e06
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-02 16:17:23 +0100

    Configured controller with learningpage

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/LearningPageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/LearningPageController.java
@@ -1,6 +1,40 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
 public class LearningPageController {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(LearningPageController.class);
+
+  public Button playAgainButton;
+  public Button checkSpellingButton;
+  public Button endLearningButton;
+  public Button showAnswerButton;
+  public Button continueButton;
+
+  public ProgressBar progressBar;
+
+  public Label remainingWordsLabel;
+  public Label rightWordsLabel;
+  public Label wrongWordsLabel;
+
+  public TextField solutionTextField;
+
+  public void handlePlayAgainButtonClick(){
+  }
+
+  public void handleCheckSpellingButtonClick(){
+  }
+
+  public void handleEndLearningButtonClick(){
+  }
+
+  public void handleShowAnswerButtonClick(){
+  }
+
+  public void handleContinueButtonClick(){
+  }
 }
