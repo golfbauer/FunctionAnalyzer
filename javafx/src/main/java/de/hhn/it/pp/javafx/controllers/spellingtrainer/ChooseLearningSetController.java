6137d27dbe9f661d206786e3b9dc6e2992036ecb
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-02 16:12:19 +0100

    Configured IDs and methods to handel button activation

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ChooseLearningSetController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ChooseLearningSetController.java
@@ -1,6 +1,20 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class ChooseLearningSetController {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(ChooseLearningSetController.class);
+
+  public Button startLearningButton;
+  public Button homescreenButton;
+
+  public ListView<String> learningsetListView;
+
+  public void handleStartLearningButtonClick(){
+  }
+
+  public void handleHomescreenButtonClick(){
+  }
 }
