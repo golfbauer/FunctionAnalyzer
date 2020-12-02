b5d82671e37d2356c5ef769d6d2d68b1b5c39b40
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2020-12-02 11:20:16 +0100

    Added Interface to generalize Term and FunctionElement

---
+++ components/src/main/java/de/hhn/it/pp/components/functionanalyzer/FunctionElementComponent.java
@@ -0,0 +1,17 @@
+package de.hhn.it.pp.components.functionanalyzer;
+
+public interface FunctionElementComponent {
+
+  FunctionElementComponent add(FunctionElementComponent functionElementComponent);
+
+  FunctionElementComponent subtract(FunctionElementComponent functionElementComponent);
+
+  FunctionElementComponent divide(FunctionElementComponent functionElementComponent);
+
+  FunctionElementComponent multiply(FunctionElementComponent functionElementComponent);
+
+  double evaluate(double variableValue);
+
+  FunctionElementComponent getDerivative();
+
+}
