cc69eb585090fcd275ad89d6e2691c5c9c36d37c
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-02 16:23:16 +0100

    Configured AnswerPopUpPageController with AnswerPopUpPage.fxml

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/AnswerPopUpPageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/AnswerPopUpPageController.java
@@ -1,6 +1,16 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class AnswerPopUpPageController {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(AnswerPopUpPageController.class);
+
+  public Label correctAnswerLabel;
+
+  public Button closeButton;
+
+  public void handleCloseButtonClick(){
+  }
 }
