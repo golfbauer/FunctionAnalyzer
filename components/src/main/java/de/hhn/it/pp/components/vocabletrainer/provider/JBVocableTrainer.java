0a20eec8a05986dd5dff86f27097efc1656e54be
bkammer <bkammer@stud.hs-heilbronn.de>
2020-12-02 11:28:22 +0100

    Added Exceptions to methods in JBVocabletrainer

--- components/src/main/java/de/hhn/it/pp/components/vocabletrainer/provider/JBVocableTrainer.java
+++ components/src/main/java/de/hhn/it/pp/components/vocabletrainer/provider/JBVocableTrainer.java
@@ -2,13 +2,13 @@ package de.hhn.it.pp.components.vocabletrainer.provider;
 
 import de.hhn.it.pp.components.vocabletrainer.Vocable;
 import de.hhn.it.pp.components.vocabletrainer.exceptions.VocCategoryAlreadyExistException;
+import de.hhn.it.pp.components.vocabletrainer.exceptions.VocCategoryNotFoundException;
+import de.hhn.it.pp.components.vocabletrainer.exceptions.VocableNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 public class JBVocableTrainer {
-  private static final org.slf4j.Logger logger =
-      org.slf4j.LoggerFactory.getLogger(JBVocableTrainer.class);
   private HashMap<String, List<Vocable>> trainerData = new HashMap<>();
   private int score;
 
@@ -23,25 +23,41 @@ public class JBVocableTrainer {
   public void addCategory(String categoryName, List<Vocable> voc)
       throws VocCategoryAlreadyExistException {
     if (trainerData.get(categoryName) != null) {
-      logger.debug("The VocCategory already exist");
-      throw new VocCategoryAlreadyExistException();
+      throw new VocCategoryAlreadyExistException("The VocCategory could not be added");
     }
     trainerData.put(categoryName, voc);
   }
 
-  public void deleteCategory(String categoryName) {
+  public void deleteCategory(String categoryName) throws VocCategoryNotFoundException {
+    if (trainerData.get(categoryName) == null) {
+      throw new VocCategoryNotFoundException("The VocCategory could not be deleted");
+    }
     trainerData.remove(categoryName);
   }
 
-  public void addVocable(String categoryName, Vocable voc) {
+  public void addVocable(String categoryName, Vocable voc) throws VocCategoryNotFoundException {
+    if (trainerData.get(categoryName) == null) {
+      throw new VocCategoryNotFoundException();
+    }
     trainerData.get(categoryName).add(voc);
   }
 
-  public void deleteVocable(String categoryName, Vocable voc) {
+  public void deleteVocable(String categoryName, Vocable voc)
+      throws VocCategoryNotFoundException, VocableNotFoundException {
+    if (trainerData.get(categoryName) == null) {
+      throw new VocCategoryNotFoundException();
+    }
+    if (!trainerData.get(categoryName).contains(voc)) {
+      throw new VocableNotFoundException(
+          "The Vocable was not found in the category " + categoryName);
+    }
     trainerData.get(categoryName).remove(voc);
   }
 
   public List<Vocable> getVocableList(String categoryName) {
+    if (trainerData.get(categoryName) == null) {
+      throw new VocCategoryNotFoundException();
+    }
     return trainerData.get(categoryName);
   }
 
