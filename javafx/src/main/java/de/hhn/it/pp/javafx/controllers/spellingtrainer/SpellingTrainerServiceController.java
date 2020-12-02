9f46d03f2ef763c455740751c5224818e48a72db
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-02 15:49:30 +0100

    Configured button handel methods

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/SpellingTrainerServiceController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/SpellingTrainerServiceController.java
@@ -1,6 +1,13 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
+import javafx.scene.control.Button;
+
 public class SpellingTrainerServiceController {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(SpellingTrainerServiceController.class);
+
+  public Button startButton;
+
+  public void handleStartButtonClick(){
+  }
 }
