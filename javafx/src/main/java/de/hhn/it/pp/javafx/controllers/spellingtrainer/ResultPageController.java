90ae6112bd12facd5a38229e723ac86dbcb72674
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-02 16:33:15 +0100

    Configured ResultPageController with ResutlPage.fxml

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ResultPageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ResultPageController.java
@@ -1,6 +1,20 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+
 public class ResultPageController {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(ResultPageController.class);
+
+  public PieChart pieChart;
+
+  public Button homescreenButton;
+  public Button continueLearningButton;
+
+  public void handleHomescreenButtonClick(){
+  }
+
+  public void handleContinueLearningButtonClick(){
+  }
 }
