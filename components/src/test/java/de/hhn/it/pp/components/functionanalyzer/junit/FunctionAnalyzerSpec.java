d3d2ae9217810021fd17f454c67d7de0fc23f3dd
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2020-12-02 15:02:18 +0100

    Implemented more test for readFunction

--- components/src/test/java/de/hhn/it/pp/components/functionanalyzer/junit/FunctionAnalyzerSpec.java
+++ components/src/test/java/de/hhn/it/pp/components/functionanalyzer/junit/FunctionAnalyzerSpec.java
@@ -13,22 +13,44 @@ public class FunctionAnalyzerSpec {
 
   FunctionAnalyzer functionAnalyzer;
   Function first;
+  Function second;
 
   @BeforeEach
   void init(){
     functionAnalyzer = new FunctionAnalyzer();
     first = new Function(
-        new FunctionElement(Operator.ADD, new Term(new Term(-2.0), 2.0, "x")),
-        new FunctionElement(Operator.ADD, new Term(new Term(1.0), -3.0, "x")),
-        new FunctionElement(Operator.ADD, new Term(-2.0)),
-        new FunctionElement(Operator.ADD, new Term(4)));
+        new FunctionElement(Operator.ADD, new Term(new Term(1.0), 2.0, "x")),
+            new FunctionElement(Operator.MULTIPLY,
+                new Term(new Term(1), 3, "x"),
+                new FunctionElement(Operator.ADD,
+                    new Term(new Term(1), 5),
+                    new FunctionElement(Operator.ADD,
+                        new Term(new Term(1), 7), new Term(new Term(1), 9)
+                    ))));
+    second = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(2), -2, "x")),
+            new FunctionElement(Operator.ADD, new Term(new Term(1), 7.0/8.0, "x")),
+            new FunctionElement(Operator.MULTIPLY,
+                    new Term(new Term(1), 5),
+                    new Term(new Term(1), 9, "x")),
+                new FunctionElement(Operator.ADD,
+                    new Term(new Term(1), -7.0/9.0))
+                    );
   }
 
-  @Test
+   @Test
   void functionCreatedFromString() {
-    Function function = functionAnalyzer.readFunction("-3^2*x^2 + 3*-5/x*(3*x+9)+16/8");
+    Function function = functionAnalyzer.readFunction("2*x*(3*x+(5+(7+9)))");
     assertEquals(first.toString(),
         function.toString(),
         () -> "Function should be created");
+  }
+
+  @Test
+  void FunctionCreatedFromStringCheckEverything() {
+    Function function = functionAnalyzer.readFunction("-2*x^2 + 7/8*x*(5 + 9*x) - 7/9");
+    assertEquals(second.toString(),
+        function.toString(),
+        () -> "Function should be crated");
   }
 }
