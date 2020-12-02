ee4e6042140c8ee7e9f0dd1d6873ff872c607e1d
Ansgar Koopmann <koopmann@stud.hs-heilbronn.de>
2020-12-02 11:45:41 +0100

    Reformat

--- components/src/main/java/de/hhn/it/pp/components/functionanalyzer/Term.java
+++ components/src/main/java/de/hhn/it/pp/components/functionanalyzer/Term.java
@@ -1,10 +1,11 @@
 package de.hhn.it.pp.components.functionanalyzer;
 
-import java.util.Objects;
 import de.hhn.it.pp.components.functionanalyzer.exceptions.ValueNotDefinedException;
+import java.util.Objects;
+
 
 /**
- * Represent a single term inside a function
+ * Represent a single term inside a function.
  */
 public class Term {
   public static final Term E = new Term(Math.E);
@@ -16,9 +17,10 @@ public class Term {
   private String variable;
 
   /**
-   * For Terms with variable in Base
+   * For Terms with variable in Base.
+   *
    * @param exponent Term that represents exponent
-   * @param factor Factor of the variable
+   * @param factor   Factor of the variable
    * @param variable name of the variable eg. x,y...
    */
   public Term(Term exponent, double factor, String variable) {
@@ -28,9 +30,10 @@ public class Term {
   }
 
   /**
-   * For constants raised to a variable
+   * For constants raised to a variable.
+   *
    * @param exponent Term that represents exponent
-   * @param value Base value
+   * @param value    Base value
    */
   public Term(Term exponent, double value) {
     this.exponent = exponent;
@@ -38,7 +41,8 @@ public class Term {
   }
 
   /**
-   * For constants without unknown variable
+   * For constants without unknown variable.
+   *
    * @param value Value of constant
    */
   public Term(double value) {
@@ -46,22 +50,23 @@ public class Term {
   }
 
   /**
-   * Calculates the result of multiplying another Term with this Term
+   * Calculates the result of multiplying another Term with this Term.
+   *
    * @param that the Term that will be multiplied by
    * @return result of the multiplication
    * @throws ValueNotDefinedException if the Terms do not have the same variable
    */
   public Term multiplyBy(Term that) throws ValueNotDefinedException {
-    if (!Objects.equals(this.variable, that.variable) &&
-        (this.variable != null) == (that.variable != null)){
+    if (!Objects.equals(this.variable, that.variable)
+        && (this.variable != null) == (that.variable != null)) {
       throw new ValueNotDefinedException("Cannot multiply Term with different variables");
     }
-    if ((this.variable == null) && (that.variable == null)){
+    if ((this.variable == null) && (that.variable == null)) {
       return new Term(this.value * that.value);
-    } else if (this.variable == null){
-      return new Term(that.exponent, this.value * that.factor ,that.variable);
-    } else if (that.variable == null){
-      return new Term(this.exponent, this.factor * that.value ,this.variable);
+    } else if (this.variable == null) {
+      return new Term(that.exponent, this.value * that.factor, that.variable);
+    } else if (that.variable == null) {
+      return new Term(this.exponent, this.factor * that.value, this.variable);
     } else {
       return new Term(this.exponent.add(that.exponent),
           this.factor * that.factor, this.variable);
@@ -69,13 +74,14 @@ public class Term {
   }
 
   /**
-   * Calculates the result of adding another Term to this Term
+   * Calculates the result of adding another Term to this Term.
+   *
    * @param that the term that will be added
    * @return result of the addition
    * @throws ValueNotDefinedException if the Terms are not equal according to {@link #structurallyEqual}
    */
   public Term add(Term that) throws ValueNotDefinedException {
-    if (!this.structurallyEqual(that)){
+    if (!this.structurallyEqual(that)) {
       throw new ValueNotDefinedException("Cannot add Terms with different structure");
     }
     if (this.variable != null) {
@@ -86,10 +92,11 @@ public class Term {
   }
 
   /**
-   * Calculate the Derivative of Term
+   * Calculate the Derivative of Term.
+   *
    * @return Derivative of the Term
    */
-  protected Term getDerivative(){
+  protected Term getDerivative() {
     return null;
   }
 
@@ -144,10 +151,10 @@ public class Term {
       return false;
     }
     Term term = (Term) o;
-    return Double.compare(term.value, value) == 0 &&
-        Double.compare(term.factor, factor) == 0 &&
-        Objects.equals(exponent, term.exponent) &&
-        Objects.equals(variable, term.variable);
+    return Double.compare(term.value, value) == 0
+        && Double.compare(term.factor, factor) == 0
+        && Objects.equals(exponent, term.exponent)
+        && Objects.equals(variable, term.variable);
   }
 
   @Override
@@ -156,12 +163,13 @@ public class Term {
   }
 
   /**
-   * Checks if two Terms are equal in structure, meaning they have the same variable and exponent
+   * Checks if two Terms are equal in structure, meaning they have the same variable and exponent.
+   *
    * @param that the Term to compare against
    * @return {@code true} if both variable and exponents are equal {@code false} if not
    */
   public boolean structurallyEqual(Term that) {
-    if (this.variable != null && that.variable != null){
+    if (this.variable != null && that.variable != null) {
       return this.variable.equals(that.variable) && this.exponent == that.exponent;
     } else if (this.variable == null && that.variable == null) {
       return this.exponent == that.exponent;
