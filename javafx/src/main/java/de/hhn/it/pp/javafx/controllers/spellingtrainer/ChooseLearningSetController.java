3b7d6d9ca387f2a10c74450c8f3591e378fdd784
Simon Groezinger <sgroezin@stud.hs-heilbronn.de>
2020-12-03 12:04:29 +0100

    Added methods to change the active scene

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ChooseLearningSetController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/spellingtrainer/ChooseLearningSetController.java
@@ -1,10 +1,14 @@
 package de.hhn.it.pp.javafx.controllers.spellingtrainer;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.StackPane;
 
 public class ChooseLearningSetController {
   private static final org.slf4j.Logger logger =
@@ -13,12 +17,29 @@ public class ChooseLearningSetController {
   public Button startLearningButton;
   public Button homescreenButton;
 
+  public StackPane scenePane;
+
   public ListView<String> learningsetListView;
 
-  public void handleStartLearningButtonClick(){
+  public void handleStartLearningButtonClick(ActionEvent event) throws IOException {
+    loadPane(event);
+    setScenePane("spellingtrainer/LearningPage");
   }
 
-  public void handleHomescreenButtonClick(){
+  public void handleHomescreenButtonClick(ActionEvent event) throws IOException {
+    loadPane(event);
+    setScenePane("SpellingTrainerService");
+  }
 
+  public void setScenePane(String url) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/" + url + ".fxml"));
+    scenePane.getChildren().clear();
+    scenePane.getChildren().add(root);
+  }
+
+  public void loadPane(ActionEvent event) throws IOException {
+    Node node = (Node) event.getSource();
+    Scene scene = node.getScene();
+    scenePane = (StackPane) scene.lookup("#scenePane");
   }
 }
