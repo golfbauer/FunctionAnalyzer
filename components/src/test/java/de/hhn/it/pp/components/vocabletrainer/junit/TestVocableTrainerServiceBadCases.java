6ddb7cfe1e0be35fb4da71c714435896b723e65a
bkammer <bkammer@stud.hs-heilbronn.de>
2020-12-03 15:31:09 +0100

    Created TestVocableTrainerServiceBadCases class

---
+++ components/src/test/java/de/hhn/it/pp/components/vocabletrainer/junit/TestVocableTrainerServiceBadCases.java
@@ -0,0 +1,8 @@
+package de.hhn.it.pp.components.vocabletrainer.junit;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Test for VocableTrainerService bad cases")
+public class TestVocableTrainerServiceBadCases {
+  
+}
