ac0af29831a2bd21e65d13776e112aea31ec6daf
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2020-12-03 16:46:14 +0100

    Added setFunctionEqualZero method and support methods

--- components/src/main/java/de/hhn/it/pp/components/functionanalyzer/Function.java
+++ components/src/main/java/de/hhn/it/pp/components/functionanalyzer/Function.java
@@ -39,4 +39,92 @@ public class Function extends ArrayList<FunctionElement> {
     return builder.toString();
 
   }
+
+  public List<Double> setFunctionEqualZero() {
+    double maxExponent = 0;
+    ArrayList<Double> result = new ArrayList<>();
+    for (FunctionElement functionElement : this) {
+      if (maxExponent < functionElement.getMaxExponent()) {
+        maxExponent = functionElement.getMaxExponent();
+      }
+    }
+    if (maxExponent == 0) {
+      if (((Term) this.get(0).getComponents().get(0)).getValue() == 0) {
+        result.add(0.0);
+        return result;
+      }
+      return null;
+    } else if (maxExponent == 1) {
+      result.add(setLinearFunctionEqualZero());
+      return result;
+    } else if (maxExponent == 2) {
+      result = setSquareFunctionEqualZero();
+      return result;
+    }
+    return new ArrayList<>();
+  }
+
+  public double setLinearFunctionEqualZero() {
+    double result;
+    if (this.size() == 2) {
+      result = ((Term) this.get(1).getComponents().get(0)).getValue()
+              / ((Term) this.get(0).getComponents().get(0)).getFactor();
+      result = result * (-1);
+      return result;
+    } else {
+      return 0;
+    }
+  }
+
+  public ArrayList<Double> setSquareFunctionEqualZero() {
+    ArrayList<Double> result = new ArrayList<>();
+    double p;
+    double q;
+    if (this.size() == 3) {
+      p = ((Term) this.get(1).getComponents().get(0)).getFactor()
+              / ((Term) this.get(0).getComponents().get(0)).getFactor();
+      q = ((Term) this.get(2).getComponents().get(0)).getValue()
+              / ((Term) this.get(0).getComponents().get(0)).getFactor();
+      return pqFormel(p, q);
+    } else if (((Term) this.get(1).getComponents().get(0)).getExponent() != null) {
+      p = ((Term) this.get(1).getComponents().get(0)).getFactor()
+              / ((Term) this.get(0).getComponents().get(0)).getFactor();
+      q = 0;
+      return pqFormel(p, q);
+    } else {
+      q = ((Term) this.get(1).getComponents().get(0)).getValue()
+              / ((Term) this.get(0).getComponents().get(0)).getFactor();
+      if (q > 0) {
+        return null;
+      } else {
+        q = q * (-1);
+        q = Math.sqrt(q);
+        result.add(q);
+        result.add(q * -1);
+        return result;
+      }
+    }
+  }
+
+  public ArrayList<Double> pqFormel(double p, double q) {
+    ArrayList<Double> result = new ArrayList<>();
+    double discriminant;
+    discriminant = (p / 2) * (p / 2) - q;
+    if (discriminant >= 0) {
+      double x1;
+      double x2;
+      x1 = -(p / 2) + Math.sqrt(discriminant);
+      x2 = -(p / 2) - Math.sqrt(discriminant);
+      if (x1 == x2) {
+        result.add(x1);
+        return result;
+      } else {
+        result.add(x1);
+        result.add(x2);
+        return result;
+      }
+    } else {
+      return null;
+    }
+  }
 }
--- components/src/test/java/de/hhn/it/pp/components/functionanalyzer/junit/FunctionSpec.java
+++ components/src/test/java/de/hhn/it/pp/components/functionanalyzer/junit/FunctionSpec.java
@@ -1,6 +1,8 @@
 package de.hhn.it.pp.components.functionanalyzer.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 import de.hhn.it.pp.components.functionanalyzer.Function;
 import de.hhn.it.pp.components.functionanalyzer.FunctionElement;
 import de.hhn.it.pp.components.functionanalyzer.Operator;
@@ -11,13 +13,22 @@ import org.junit.jupiter.api.Test;
 public class FunctionSpec {
 
   Function normal;
+  Function linear;
+  Function simple;
 
   @BeforeEach
   void init() {
+    simple = new Function(
+            new FunctionElement(Operator.ADD, new Term(5))
+    );
     normal = new Function(
         new FunctionElement(Operator.ADD, new Term(new Term(2), 5, "x")),
         new FunctionElement(Operator.ADD, new Term(new Term(1), -3, "x")),
         new FunctionElement(Operator.ADD, new Term(8))
+    );
+    linear = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(1), -3, "x")),
+            new FunctionElement(Operator.ADD, new Term(9))
     );
   }
 
@@ -29,5 +40,67 @@ public class FunctionSpec {
     );
     Function actual = normal.getDerivative();
     assertEquals(expected.toString(), actual.toString(), "Sould be the derivative of Function");
+  }
+
+  @Test
+  void getValueFromFunctionWithoutX() {
+    assertEquals(null, simple.setFunctionEqualZero(), "Should produce null since its never gonna" +
+            " Intercept with x Axis");
+  }
+
+  @Test
+  void getXValueFromLinearFunction() {
+    List<Double> actual  = linear.setFunctionEqualZero();
+    List<Double> expected = new ArrayList<>();
+    expected.add(3.0);
+    assertEquals(expected, actual, "Should get the x Values for Function equal to 0");
+  }
+
+  @Test
+  void getXValueForSingleLinearFunction() {
+    List<Double> actual = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(1), 4, "x"))
+    ).setFunctionEqualZero();
+    List<Double> expected = new ArrayList<>();
+    expected.add(0.0);
+    assertEquals(expected, actual, "Should get the x Values for Function equal to 0");
+  }
+
+  @Test
+  void getValueForSquareFunction() {
+    List<Double> actual = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(2), 3, "x")),
+            new FunctionElement(Operator.ADD, new Term(new Term(1), 5, "x")),
+            new FunctionElement(Operator.ADD, new Term(1))
+    ).setFunctionEqualZero();
+    List<Double> expected = new ArrayList<>();
+    expected.add(-0.2324081207560017);
+    expected.add(-1.434258545910665);
+    assertEquals(expected, actual, "Should get the x Values for Function equal to 0");
+  }
+
+  @Test
+  void getValueForSquareWithoutPFunction() {
+    List<Double> actual = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(2), 3, "x")),
+            new FunctionElement(Operator.ADD, new Term(new Term(1), 5, "x"))
+    ).setFunctionEqualZero();
+    List<Double> expected = new ArrayList<>();
+    expected.add(0.0);
+    expected.add(-1.6666666666666667);
+    assertEquals(expected, actual, "Should get the x Values for Function equal to 0");
+  }
+
+
+  @Test
+  void getValueForSquareWithoutQFunction() {
+    List<Double> actual = new Function(
+            new FunctionElement(Operator.ADD, new Term(new Term(2), 5, "x")),
+            new FunctionElement(Operator.ADD, new Term(-8))
+    ).setFunctionEqualZero();
+    List<Double> expected = new ArrayList<>();
+    expected.add(1.2649110640673518);
+    expected.add(-1.2649110640673518);
+    assertEquals(expected, actual, "Should get the x Values for Function equal to 0");
   }
 }
