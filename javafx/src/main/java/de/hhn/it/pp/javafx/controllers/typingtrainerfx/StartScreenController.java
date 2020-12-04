98d280d12ec6f619212609f7d30ff524a3c16ae6
Robert Pistea <robert.pistea@gmail.com>
2020-12-04 10:58:53 +0100

    Changed Container for Listview in StartScreen.

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/typingtrainerfx/StartScreenController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/typingtrainerfx/StartScreenController.java
@@ -24,7 +24,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
