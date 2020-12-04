b9f61e6b73fe75fa23a99bab9549f6c76ff3f4e0
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-12-04 13:13:57 +0100

    add module in main

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/HomepageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/HomepageController.java
@@ -3,24 +3,63 @@ package de.hhn.it.pp.javafx.controllers.vocabletrainer;
 
 import de.hhn.it.pp.components.vocabletrainer.LearningState;
 import de.hhn.it.pp.components.vocabletrainer.provider.JbVocableTrainerService;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class HomepageController implements Initializable {
   JbVocableTrainerService jbVocableTrainerService = new JbVocableTrainerService();
   LearningState learningState = new LearningState();
 
+  public AnchorPane scenePane;
   @FXML
   ListView<String> categoryListView;
+
 
   public void initialize(URL location, ResourceBundle resources) {
     List<String> categories = jbVocableTrainerService.getVocCategories();
     for (int i = 0; categories.size() > i; i++) {
       categoryListView.getItems().add(jbVocableTrainerService.getVocCategories().get(i));
     }
+  }
+
+  /**
+   * Method to set the next pane.
+   *
+   * @param url reference to the next pane (.fxml file)
+   * @throws IOException is thrown, when the referenced .fxml file does not exist
+   */
+  public void setScenePane(String url) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/" + url + ".fxml"));
+    scenePane.getChildren().clear();
+    scenePane.getChildren().add(root);
+  }
+
+  /**
+   * Method to load the current pane.
+   *
+   * @param event triggered by clicking a button, which switches the page
+   * @throws IOException is thrown, when the referenced .fxml file does not exist
+   */
+  public void loadPane(javafx.event.ActionEvent event) throws IOException {
+    Node node = (Node) event.getSource();
+    Scene scene = node.getScene();
+    scenePane = (AnchorPane) scene.lookup("#scenePane");
+  }
+
+  public void newCategory(ActionEvent event) throws IOException {
+    loadPane(event);
+    setScenePane("EditCreateCategoryPage");
   }
 }
