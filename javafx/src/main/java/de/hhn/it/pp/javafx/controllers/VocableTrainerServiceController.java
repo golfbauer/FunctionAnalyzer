80710daeba9c48c117e7776f8e38491e183442f3
bkammer <bkammer@stud.hs-heilbronn.de>
2020-12-04 19:38:52 +0100

    Button to start page with action

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/VocableTrainerServiceController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/VocableTrainerServiceController.java
@@ -1,22 +1,28 @@
 package de.hhn.it.pp.javafx.controllers;
 
 import de.hhn.it.pp.components.vocabletrainer.provider.JbVocableTrainerService;
+import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 public class VocableTrainerServiceController extends Controller implements Initializable {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(VocableTrainerServiceController.class);
 
-  private Map<String, Controller> keyToControllerMap;
+  @FXML
+  AnchorPane scenePane;
 
-  public VocableTrainerServiceController(){
+  public VocableTrainerServiceController() {
     logger.debug("VocableTrainerServiceController created.");
     JbVocableTrainerService jbVocableTrainerService = new JbVocableTrainerService();
-    keyToControllerMap = new HashMap<>();
   }
 
   /**
@@ -30,5 +36,34 @@ public class VocableTrainerServiceController extends Controller implements Initi
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
+  }
+
+  /**
+   * Method to set the next pane.
+   *
+   * @param url reference to the next pane (.fxml file)
+   * @throws IOException is thrown, when the referenced .fxml file does not exist
+   */
+  public void setScenePane(String url) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/" + url + ".fxml"));
+    scenePane.getChildren().clear();
+    scenePane.getChildren().add(root);
+  }
+
+  public void startVocableTrainer(ActionEvent event) throws IOException {
+    loadPane(event);
+    setScenePane("/vocabletrainer/Homepage");
+  }
+
+  /**
+   * Method to load the current pane.
+   *
+   * @param event triggered by clicking a button, which switches the page
+   * @throws IOException is thrown, when the referenced .fxml file does not exist
+   */
+  public void loadPane(ActionEvent event) throws IOException {
+    Node node = (Node) event.getSource();
+    Scene scene = node.getScene();
+    scenePane = (AnchorPane) scene.lookup("#scenePane");
   }
 }
