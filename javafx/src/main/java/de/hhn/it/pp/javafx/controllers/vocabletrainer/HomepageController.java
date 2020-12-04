4900475edf0e2789bd9c3bc9dab1f1be65cb8508
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-12-04 12:42:23 +0100

    HompageController with Initialize

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/HomepageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/HomepageController.java
@@ -1,5 +1,26 @@
 package de.hhn.it.pp.javafx.controllers.vocabletrainer;
 
-public class HomepageController {
 
+import de.hhn.it.pp.components.vocabletrainer.LearningState;
+import de.hhn.it.pp.components.vocabletrainer.provider.JbVocableTrainerService;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+public class HomepageController implements Initializable {
+  JbVocableTrainerService jbVocableTrainerService = new JbVocableTrainerService();
+  LearningState learningState = new LearningState();
+
+  @FXML
+  ListView<String> categoryListView;
+
+  public void initialize(URL location, ResourceBundle resources) {
+    List<String> categories = jbVocableTrainerService.getVocCategories();
+    for (int i = 0; categories.size() > i; i++) {
+      categoryListView.getItems().add(jbVocableTrainerService.getVocCategories().get(i));
+    }
+  }
 }
