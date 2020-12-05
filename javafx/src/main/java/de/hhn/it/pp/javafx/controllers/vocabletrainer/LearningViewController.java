926b1a2de974982fa7669071b0fe7d373e20f2f4
bkammer <bkammer@stud.hs-heilbronn.de>
2020-12-05 13:04:56 +0100

    Extracted alert messages

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/LearningViewController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/LearningViewController.java
@@ -1,10 +1,11 @@
 package de.hhn.it.pp.javafx.controllers.vocabletrainer;
 
+import static de.hhn.it.pp.javafx.controllers.VocableTrainerServiceController.jbVocableTrainerService;
 import static de.hhn.it.pp.javafx.controllers.vocabletrainer.HomepageController.cateSaver;
 import static de.hhn.it.pp.javafx.controllers.vocabletrainer.HomepageController.levenshtein;
 import static de.hhn.it.pp.javafx.controllers.vocabletrainer.VocabularyViewController.toLearnList;
 import static de.hhn.it.pp.javafx.controllers.vocabletrainer.VocabularyViewController.vocEdit;
-import static de.hhn.it.pp.javafx.controllers.VocableTrainerServiceController.jbVocableTrainerService;
+
 
 import de.hhn.it.pp.components.vocabletrainer.Vocable;
 import de.hhn.it.pp.components.vocabletrainer.exceptions.VocCategoryNotFoundException;
@@ -22,6 +23,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -31,7 +33,7 @@ public class LearningViewController implements Initializable {
       org.slf4j.LoggerFactory.getLogger(VocableTrainerServiceController.class);
   public static List<Vocable> skippedAndFailed = new ArrayList<>();
   public static boolean checker;
-  public static int listPosition;
+  public static int vocPosInCategory;
   public static String userText;
   @FXML
   AnchorPane scenePane;
@@ -56,42 +58,69 @@ public class LearningViewController implements Initializable {
     }
   }
 
-  public void checkVocable(ActionEvent event)
-      throws IOException, VocCategoryNotFoundException, VocableNotFoundException {
-    if (toLearnList.size() == 0) {
-      loadPane(event);
-      setScenePane("/vocabletrainer/EndscreenController");
-    } else {
-      for (listPosition = 0;
-           listPosition <= jbVocableTrainerService.getVocabulary(cateSaver).size();
-           listPosition++) {
-        if (jbVocableTrainerService.getVocable(listPosition, cateSaver).getLearningWord()
-            .equals(toLearnList.get(0).getLearningWord())) {
-          checker =
-              jbVocableTrainerService
-                  .checkVocable(textFieldInput.getText(), listPosition, cateSaver, levenshtein);
-          userText = textFieldInput.getText();
-          if (checker) {
-            toLearnList.remove(0);
-            loadPane(event);
-            setScenePane("/vocabletrainer/LearningNotificationController");
-          } else {
-            skippedAndFailed.add(toLearnList.get(0));
-            toLearnList.remove(0);
-            loadPane(event);
-            setScenePane("/vocabletrainer/LearningNotificationController");
-          }
-        }
-      }
+  public void checkVocable(ActionEvent event) throws IOException {
+    // Get vocable
+    Vocable vocable = null;
+    try {
+      vocable = jbVocableTrainerService.getVocable(vocPosInCategory, cateSaver);
+    } catch (VocCategoryNotFoundException e) {
+      vocCategoryNotFoundAlert();
+      return;
+    } catch (VocableNotFoundException e) {
+      vocableNotFoundAlert();
+      return;
     }
+    // Get word from textField
+    String wordToCompare = textFieldInput.getText().trim();
+    boolean isCorrect = false;
+    try {
+      // Check vocable correctness
+      isCorrect = jbVocableTrainerService
+          .checkVocable(wordToCompare, vocPosInCategory, cateSaver, levenshtein);
+    } catch (VocCategoryNotFoundException e) {
+      vocCategoryNotFoundAlert();
+      return;
+    } catch (VocableNotFoundException e) {
+      vocableNotFoundAlert();
+      return;
+    }
+    if (isCorrect) {
+      // Vocable is correct
+      // Show correct word to compare if levenshtein was used
+
+    } else {
+      // Vocable is false
+      // Add vocable to list of false words
+    }
+  }
+
+  private void vocableNotFoundAlert() {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("Warning Dialog");
+    alert.setHeaderText("Vocable not found");
+    alert.setContentText("Please cancel");
+    alert.showAndWait();
+  }
+
+  private void vocCategoryNotFoundAlert() {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("Warning Dialog");
+    alert.setHeaderText("Category not found");
+    alert.setContentText("Please cancel");
+    alert.showAndWait();
+  }
+
+  private void loadScene(ActionEvent event, String sceneUrl) throws IOException {
+    loadPane(event);
+    setScenePane(sceneUrl);
   }
 
   public void cancel(ActionEvent event) throws IOException {
     cateSaver = null;
     vocEdit = null;
-    listPosition = 0;
+    vocPosInCategory = 0;
     loadPane(event);
-    setScenePane("/vocabletrainer/Homepage");
+    setScenePane("/vocabletrainer/Endscreen");
   }
 
   /**
