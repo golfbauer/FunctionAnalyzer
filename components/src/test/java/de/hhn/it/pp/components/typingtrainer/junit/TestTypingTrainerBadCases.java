e15b787a2770c80cf45bdfa1761e8353b3d57d3f
Tobias Maraci <tmaraci@stud.hs-heilbronn.de>
2020-12-06 21:12:10 +0100

    Worked on badCases UnitTest

--- components/src/test/java/de/hhn/it/pp/components/typingtrainer/junit/TestTypingTrainerBadCases.java
+++ components/src/test/java/de/hhn/it/pp/components/typingtrainer/junit/TestTypingTrainerBadCases.java
@@ -3,7 +3,14 @@ package de.hhn.it.pp.components.typingtrainer.junit;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import de.hhn.it.pp.components.typingtrainer.TypingTrainerService;
+import de.hhn.it.pp.components.typingtrainer.WordNotFoundException;
+import de.hhn.it.pp.components.typingtrainer.provider.ProviderTypingTrainer;
+import de.hhn.it.pp.components.typingtrainer.Feedback;
+import de.hhn.it.pp.components.typingtrainer.SaveData;
+import de.hhn.it.pp.components.typingtrainer.SaveLoad;
+import de.hhn.it.pp.components.typingtrainer.TypingTrainerDescriptor;
+import de.hhn.it.pp.components.typingtrainer.PracticeText;
+import de.hhn.it.pp.components.typingtrainer.FileReader;
 import de.hhn.it.pp.components.typingtrainer.WordNotFoundException;
 import java.io.File;
 import java.io.IOException;
@@ -18,8 +25,17 @@ public class TestTypingTrainerBadCases {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(TestTypingTrainerBadCases.class);
 
+  ProviderTypingTrainer TypingTrainerService;
+  String audioFile;
+  String[] testPractice;
+  String[] testTyped;
+  Feedback testback;
+
+
   @BeforeEach
   void initialize() throws IOException, WordNotFoundException, LineUnavailableException, UnsupportedAudioFileException, InterruptedException{
+  TypingTrainerService = new ProviderTypingTrainer();
+  audioFile = "javafx/src/main/resources/typingTrainerFiles/8BIT RETRO Beep.mp3";
 
   }
 
