6b110aa0fb343fa4fc1e6a7e3fc2776fe75b2d40
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-12-07 12:44:36 +0100

    vocabularyView updated

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/VocabularyViewController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/VocabularyViewController.java
@@ -89,7 +89,7 @@ public class VocabularyViewController implements Initializable {
       Alert alert = new Alert(Alert.AlertType.INFORMATION);
       alert.setTitle("Information Dialog");
       alert.setHeaderText(null);
-      alert.setContentText("No Vocab to learn!");
+      alert.setContentText("No Vocabulary to learn!");
 
       alert.showAndWait();
     } else {
