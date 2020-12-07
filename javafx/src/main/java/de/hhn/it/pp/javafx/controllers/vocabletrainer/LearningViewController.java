19ee6f255f2a9fb66322f380c698900cab581e11
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-12-07 13:16:14 +0100

    setEditable for textfield in LearningView

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/LearningViewController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/LearningViewController.java
@@ -134,7 +134,7 @@ public class LearningViewController implements Initializable {
     }
     displayCorrectWord.setText("The correct word: " + vocable);
     skipButton.setDisable(true);
-    textFieldInput.setDisable(true);
+    textFieldInput.setEditable(false);
   }
 
   public void cancel(ActionEvent event) throws IOException {
@@ -240,7 +240,7 @@ public class LearningViewController implements Initializable {
     successFail.setText("");
     displayCorrectWord.setText("");
     skipButton.setDisable(false);
-    textFieldInput.setDisable(false);
+    textFieldInput.setEditable(true);
     notificationState = false;
   }
 }
