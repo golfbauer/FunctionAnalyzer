d435d3390477b6fec4e10203602fd4af40408522
Jathurshan Sothilingam <Jathujs@users.noreply.github.com>
2020-12-07 12:30:02 +0100

    Key press enter for save in EditVocablePageController

--- javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/EditVocablePageController.java
+++ javafx/src/main/java/de/hhn/it/pp/javafx/controllers/vocabletrainer/EditVocablePageController.java
@@ -21,7 +21,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 
 public class EditVocablePageController implements Initializable {
@@ -34,6 +36,8 @@ public class EditVocablePageController implements Initializable {
   TextField learningWordField;
   @FXML
   TextField translationWordsField;
+  @FXML
+  Button saveVocButton;
 
   public void saveVocable(ActionEvent event) throws IOException {
     logger.debug("Save button is pressed.");
@@ -158,6 +162,13 @@ public class EditVocablePageController implements Initializable {
    */
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    saveVocButton.setDefaultButton(true);
+    saveVocButton.setOnKeyPressed(event -> {
+          if (event.getCode().equals(KeyCode.ENTER)) {
+            saveVocButton.fire();
+          }
+        }
+    );
     if (vocEdit != null) {
       learningWordField.setText(vocEdit.getLearningWord());
       // ToDo display translation words with commas in the textbox
