78507c0f4e663074ae68253abebb7fa57fe198b4
Fabian Binnen <fbinnen@stud.hs-heilbronn.de>
2020-12-18 00:51:48 +0100

    resolving final checkstyle errors

--- components/src/main/java/de/hhn/it/pp/components/mathtrainer/MathTrainer.java
+++ components/src/main/java/de/hhn/it/pp/components/mathtrainer/MathTrainer.java
@@ -12,168 +12,188 @@ import java.util.List;
 public interface MathTrainer {
 
   /**
-  * return current user name.
-  * @return username.
-  */
+   * return current user name.
+   *
+   * @return username.
+   */
   String getUsername();
 
   /**
-  * sets the current user name to the value that was given as parameter.
-  * @param username name of the player
-  * @throws IllegalParameterException if input is invalid
-  */
+   * sets the current user name to the value that was given as parameter.
+   *
+   * @param username name of the player
+   * @throws IllegalParameterException if input is invalid
+   */
   void setUsername(String username) throws IllegalParameterException;
 
   /**
-  * returns current Section value.
-  * @return section
-  */
+   * returns current Section value.
+   *
+   * @return section
+   */
   Section getSection();
 
   /**
-  * sets the section to the value specified in the parameter.
-  * @param section calculation mode
-  */
+   * sets the section to the value specified in the parameter.
+   *
+   * @param section calculation mode
+   */
   void setSection(Section section);
 
   /**
-  * returns the currently active difficulty level.
-  * @return difficulty
-  */
+   * returns the currently active difficulty level.
+   *
+   * @return difficulty
+   */
   Difficulty getDifficulty();
 
   /**
-  * sets the difficulty level equal to the parameter value.
-  * @param difficulty one of 3 difficulties
-  * @throws IllegalParameterException if difficulty does not match any present values
-  */
+   * sets the difficulty level equal to the parameter value.
+   *
+   * @param difficulty one of 3 difficulties
+   * @throws IllegalParameterException if difficulty does not match any present values
+   */
   void setDifficulty(Difficulty difficulty) throws IllegalParameterException;
 
   /**
-  * returns the number of decimals after the period specified by the user.
-  * @return decimalPlace
-  */
+   * returns the number of decimals after the period specified by the user.
+   *
+   * @return decimalPlace
+   */
   int getDecimalPlace();
 
   /**
-  * sets the number of decimals after the period to the value specified inside the parameter.
-  * @param decimalPlace decimal count after period
-  * @throws IllegalParameterException if negative numbers are entered
-  */
+   * sets the number of decimals after the period to the value specified inside the parameter.
+   *
+   * @param decimalPlace decimal count after period
+   * @throws IllegalParameterException if negative numbers are entered
+   */
   void setDecimalPlace(int decimalPlace) throws IllegalParameterException;
 
   /**
-  * returns the current user score.
-  * @return userscore.
-  */
+   * returns the current user score.
+   *
+   * @return userscore.
+   */
   int getUserScore();
 
   /**
-  * allowes to reset or change the user score to the value specified in the parameter.
-  * @param number replaces userscore value
-  * @throws IllegalParameterException if userscore is negative through this methodcall
-  */
+   * allowes to reset or change the user score to the value specified in the parameter.
+   *
+   * @param number replaces userscore value
+   * @throws IllegalParameterException if userscore is negative through this methodcall
+   */
   void setUserScore(int number) throws IllegalParameterException;
 
   /**
-  * changes the boolean value of timeisup.
-  * @param timeboolean boolean value true when time is up
-  */
+   * changes the boolean value of timeisup.
+   *
+   * @param timeboolean boolean value true when time is up
+   */
   void setTimeIsUp(boolean timeboolean);
 
   /**
-  * returns the boolean value of timeisup.
-  * @return timeisup
-  */
+   * returns the boolean value of timeisup.
+   *
+   * @return timeisup
+   */
   boolean getTimeIsUp();
 
   /**
    * set the integer value inside the MathTrainer class.
+   *
    * @param inturn integer value
    */
   void setInTurn(int inturn);
 
   /**
-  * returns the integer value of the local datafield inturn.
-  * @return inturn
-  */
+   * returns the integer value of the local datafield inturn.
+   *
+   * @return inturn
+   */
   int getInTurn();
 
   /**
-  * changes the boolean value of warmup.
-  * @param warmupboolean boolean value to determine which mode is played
-  */
+   * changes the boolean value of warmup.
+   *
+   * @param warmupboolean boolean value to determine which mode is played
+   */
   void setWarmup(boolean warmupboolean);
 
   /**
-  * returns the boolean value of warmup.
-  * @return warmup
-  */
+   * returns the boolean value of warmup.
+   *
+   * @return warmup
+   */
   boolean getWarmup();
 
   /**
-  * adds the timebonus for fast solutions and the bonus for difficult questions to the userscore.
-  * @param timebonus bonus points for quick solutions
-  * @throws IllegalParameterException if negative values for timebonus should appear
-  */
+   * adds the timebonus for fast solutions and the bonus for difficult questions to the userscore.
+   *
+   * @param timebonus bonus points for quick solutions
+   * @throws IllegalParameterException if negative values for timebonus should appear
+   */
   void addToUserScore(int timebonus) throws IllegalParameterException;
 
   /**
-  * createTerm for generating a random term.
-  * @return the random generated term
-  */
+   * createTerm for generating a random term.
+   *
+   * @return the random generated term
+   */
   Term createTerm();
 
   /**
    * creates a new Term object and increments the inturn counter of this MathTrainer object.
+   *
    * @return generated Term object
    */
   Term nextTerm();
 
   /**
-  * solveTerm to check if a term is equal with the solution that the user entered in.
-  *
-  * @param userInput solution input from user
-  * @param term current term object that needs solving
-  * @return returns true or false depending on whether or not the term has been solved successfully.
-  * @exception IllegalArgumentException if invalid solutions are entered by the user.
-  */
-
+   * solveTerm to check if a term is equal with the solution that the user entered in.
+   *
+   * @param userInput solution input from user
+   * @param term current term object that needs solving
+   * @return returns true/false depending on whether or not the term has been solved successfully.
+   * @exception IllegalArgumentException if invalid solutions are entered by the user.
+   */
   boolean solveTerm(String userInput, Term term);
 
   /**
-  * createTerm for generating a random term.
-  *
-  * @param userInput solution input from user
-  * @param term current term that needs solving
-  * @param solvedInSeconds timestamp at which term was solved by user
-  * @return returns true or false depending on whether or not the term has been solved successfully.
-  * @exception IllegalArgumentException if invalid solutions are entered by user.
-  */
+   * createTerm for generating a random term.
+   *
+   * @param userInput solution input from user
+   * @param term current term that needs solving
+   * @param solvedInSeconds timestamp at which term was solved by user
+   * @return returns true/false depending on whether or not the term has been solved successfully.
+   * @exception IllegalArgumentException if invalid solutions are entered by user.
+   */
   boolean solveTerm(String userInput, Term term, int solvedInSeconds);
 
   /**
-  * adds the current object datafields name, userscore and difficulty to a list.
-  */
+   * adds the current object datafields name, userscore and difficulty to a list.
+   */
   void addToHistory();
 
   /**
-  * returns a list for further use.
-  * @return a list element
-  */
+   * returns a list for further use.
+   *
+   * @return a list element
+   */
   List<String> getHistory();
 
   /**
-  * returns solution of term if the user asks for it, note no points are added to the user score.
-  * @param term term object
-  * @return BigDecimal object of the terms solution
-  * @throws IllegalParameterException if the term is somehow invalid
-  */
+   * returns solution of term if the user asks for it, note no points are added to the user score.
+   *
+   * @param term term object
+   * @return BigDecimal object of the terms solution
+   * @throws IllegalParameterException if the term is somehow invalid
+   */
   BigDecimal helpUser(Term term) throws IllegalParameterException;
 
   /**
-  * creates a few demo string entries to be used in the GUI interface later.
-  *
-  */
+   * creates a few demo string entries to be used in the GUI interface later.
+   *
+   */
   void createDemoHistoryData();
 }
